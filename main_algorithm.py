12b6c6fb49d985fbea57c2ae1041e916aa21acb2
Leonie Bottomley <leoniegracebottomley@gmail.com>
2025-03-06 10:46:54 +0000

    Define main functions - outline call system (basic)
    No real code here, just a basic first commit

---
+++ main_algorithm.py
@@ -0,0 +1,22 @@
+
+
+# Main call point for processing the tree input images
+
+def main():
+    # Load in the model
+
+    # Segmentation 
+
+    # Skeletonisation / medial axis 
+
+    # Section brances and prune short branches
+
+    
+
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
+    # main()
---
+++ main_segmentation.py
@@ -0,0 +1,20 @@
+
+# Everything to do with training the segmentation model will be controlled from here
+# Call all parent functions from here 
+# Lines of code can be commented out as required
+
+
+def main():
+    # Preprocess dataset for training
+
+    # Train model
+
+    # Test model on custom input
+
+
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
