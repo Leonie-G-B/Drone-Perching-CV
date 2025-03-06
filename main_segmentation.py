b08ce1fa0a29b0b4f42ccc46f3c1917a289e989d
Leonie Bottomley <leoniegracebottomley@gmail.com>
2025-03-06 15:18:41 +0000

    Access point to pre-pro in the main script

--- main_segmentation.py
+++ main_segmentation.py
@@ -3,9 +3,16 @@
 # Call all parent functions from here 
 # Lines of code can be commented out as required
 
+# IMPORTS 
+import pre_training_process as PrePro
+
+
 
 def main():
     # Preprocess dataset for training
+    PrePro.main(mask_dir='yolo/dataset/masks',
+                resize_factor= 0.4,
+                dir = 'both')
 
     # Train model
 
@@ -17,4 +24,4 @@ def main():
 
 
 if __name__ == "__main__":
-    pass
+    main()
